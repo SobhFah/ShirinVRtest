7a0095eec5e004464ed1cddb7e81569aa66a1f8e
SobhFah <95325114+SobhFah@users.noreply.github.com>
2021-12-17 17:12:21 -0500

    added unity project

---
+++ dec06testVR/Assets/ummalramlshirindec16/scripts/HandAnimator.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandAnimator : MonoBehaviour
+{
+    private VRInput controller;
+    private Animator handAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<VRInput>();
+        handAnimator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller)
+        {
+            handAnimator.Play("Fist Closing", 0,
+                controller.gripValue);
+        }        
+    }
+}
